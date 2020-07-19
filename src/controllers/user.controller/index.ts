2bd7e8b2423d3830b3fd2a8e4cff4daff14fa927
Arif Surahman <as.redbios@gmail.com>
2020-07-19 20:07:43 +0700

    fix implement-validation

--- src/controllers/user.controller/index.ts
+++ src/controllers/user.controller/index.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
-import { asyncHandler, succeesResponse } from "../utils";
-import { User } from "../models";
+import {
+	asyncHandler,
+	succeesResponse,
+	validationFailResponse,
+} from "../../utils";
+import { User } from "../../models";
+import { validationResult } from "express-validator";
+
+/**
+ * for re exporting validator
+ */
+export * from "./validator";
 
 /**
  * Get all data from user table
@@ -13,6 +23,12 @@ export const get = asyncHandler(async (req: Request, res: Response) => {
  * create user to table user
  */
 export const createUser = asyncHandler(async (req: Request, res: Response) => {
+	const error = validationResult(req);
+	if (error) {
+		validationFailResponse(res, error.array());
+		return;
+	}
+
 	const user: Omit<User, "id"> = req.body;
 	succeesResponse(res, await User.create(user));
 });
