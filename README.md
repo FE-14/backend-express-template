dd899d4253486e32b2ed6a6ba1c4ad367f48e886
mojila <me.mojila@gmail.com>
2021-01-04 11:31:46 +0700

    [ADD] example full method

--- README.md
+++ README.md
@@ -1,21 +1,16 @@
 # Dokumentasi Backend Boilerplate
 
-<details>
-  <summary>Lihat Daftar Isi</summary>
- 
-  ## Daftar Isi  
- 1. Penggunaan Boilerplate
-	 1. Memulai Project baru 
- 2.
+## Daftar Isi
 
-</details>
-
-## Penggunaan Boilerplate
+1. [Memulai Project Baru](#memulai-project-baru)
+2. [Konfigurasi Database](#konfigurasi-database)
+3. Instalasi Module/Dependency Baru
 
 ### Memulai Project baru
 
+<details>
+<summary>Lihat</summary>
 Pada saat developer memulai project aplikasi baru maka diharuskan untuk melakukan clone pada boilerplate ini yang berada pada repository [Backend Express](https://gitlab.com/mv-lanius/boilerplates/backend-express.git) branch master. Setelah di clone maka perlu dilakukan penghapusan origin menggunakan perintah:
-
 > git remote remove origin
 
 Setelah menghapus origin awal maka rename file .env.example menjadi .env. Mulai instalasi module awal dengan perintah:
@@ -29,3 +24,56 @@ Setelah itu kita perlu menggantinya dengan origin repository baru untuk project 
 > git add .
 
 > git commit -m “[CREATE] project init”
+
+</details>
+
+### Konfigurasi Database
+
+<details>
+<summary>Lihat</summary>
+Sistem database yang kita gunakan yaitu PostgreSQL dan MongoDB yang masing-masing memiliki peran. PostgreSQL digunakan untuk data bersifat fixed-field yang dimana field table tidak bertambah atau berkurang secara dinamis dan digunakan untuk database bersifat relasional. Sedangkan MongoDB digunakan untuk data bersifat dynamic-property yang data property dapat bertambah atau berkurang secara dinamis.
+Pada saat membuat project baru diharapkan untuk membuat inisialisasi database kosong dengan nama sesuai nama project tersebut misalkan: ``db_pg_petro_vr``, ``db_mongo_sim_engineering``. Setelah itu kita dapat mengisi database authentication pada file ``.env`` seperti berikut:
+
+    # Global Env
+    NODE_ENV=development
+    PORT=4000
+
+    # postgres env
+    DB_USERNAME=postgres
+    DB_PASSWORD=password123
+    DB_HOST=localhost
+    DB_PORT=5432
+    DB_NAME=db_pg_contoh
+    DB_DIALECT=postgres
+
+    # mongo env
+    MONGO_DB_USERNAME=mongo
+    MONGO_DB_PASSWORD=password123
+    MONGO_DB_HOST=localhost
+    MONGO_DB_PORT=5432
+    MONGO_DB_NAME=db_mongo_contoh
+
+    # jwt for login
+    JWT_EXPIRE="7d"
+    JWT_SECRET="akucintalanius"
+
+</details>
+
+### Instalasi Module/Dependency Baru
+
+<details>
+<summary>Lihat</summary>
+Instalasi module pada boilerplate ini menggunakan package manager Yarn. sehingga diharapkan untuk semua pemakaian command npm diganti dengan yarn. Berikut command instalasi awal boilerplate dengan perintah:
+>yarn
+
+Ketika hendak menambahkan module baru maka menggunakan perintah berikut:
+
+> yarn add [nama-module]
+
+Ketika hendak menambahkan module baru bersifat development only maka menggunakan perintah berikut
+
+> yarn add -D [nama-module]
+
+</details>
+
+### Membuat Model
--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -1,15 +1,13 @@
 import { _Request } from "../interfaces";
-import { Controller, Get } from "../decorators";
+import { Controller, Delete, Get, Post, Put } from "../decorators";
 import { Response } from "express";
-import { successResponse } from "../utils";
-import Example from "../models/example.model";
+import Example, { ExampleAttributes } from "../models/example.model";
 
 const tag = "Example";
 
 @Controller("/examples")
 export default class ExampleController {
   @Get({ path: "/", tag }, {
-    request: {},
     responses: [
       {
         200: {
@@ -21,24 +19,13 @@ export default class ExampleController {
     ],
     parameters: []
   }, [])
-  public async getAll(req: _Request, res: Response): Promise<Response> {
-    try {
-      const data = await Example.findAll({});
+  public async getAll(req: _Request, res: Response): Promise<Example[]> {
+    const data = await Example.findAll({});
 
-      return successResponse({
-        res,
-        data
-      });
-    } catch (e) {
-      return res.status(500).json({
-        success: false,
-        message: e
-      });
-    }
+    return data;
   }
 
   @Get({ path: "/:id", tag }, {
-    request: {},
     responses: [
       {
         200: {
@@ -58,27 +45,135 @@ export default class ExampleController {
       }
     ]
   }, [])
-  public async getOne(req: _Request, res: Response): Promise<Response> {
-    try {
-      const { id } = req.params;
+  public async getOne(req: _Request, res: Response): Promise<Example> {
+    const { id } = req.params;
 
-      const data = await Example.findOne({
-        where: {
-          id
+    const data = await Example.findOne({
+      where: {
+        id
+      }
+    });
+
+    if (!data) throw "Data not found";
+
+    return data;
+  }
+
+  @Post({ path: "/", tag }, {
+    request: "NewExample",
+    responses: [
+      {
+        200: {
+          description: "",
+          responseType: "object",
+          schema: "Example"
         }
-      });
+      }
+    ],
+  })
+  public async create(req: _Request, res: Response): Promise<Example> {
+    const {
+      name,
+      description
+    }: ExampleAttributes = req.body;
 
-      if (!data) throw "Data not found";
+    const data = await Example.create({
+      name,
+      description
+    });
 
-      return successResponse({
-        res,
-        data
-      });
-    } catch (e) {
-      return res.status(500).json({
-        success: false,
-        message: e
-      });
-    }
+    return data;
+  }
+
+  @Put({ path: "/:id", tag }, {
+    request: "NewExample",
+    responses: [
+      {
+        200: {
+          description: "",
+          responseType: "object",
+          schema: "Example"
+        }
+      }
+    ],
+    parameters: [
+      {
+        name: "id",
+        in: "path",
+        required: true,
+        schema: {
+          type: "number"
+        }
+      }
+    ]
+  })
+  public async update(req: _Request, res: Response): Promise<Example> {
+    const {
+      id
+    } = req.params;
+    const {
+      name,
+      description
+    }: ExampleAttributes = req.body;
+
+    const update = await Example.update({
+      name,
+      description
+    }, {
+      where: {
+        id
+      }
+    });
+
+    const data = await Example.findOne({
+      where: {
+        id
+      }
+    });
+
+    return data;
+  }
+
+  @Delete({ path: "/:id", tag }, {
+    responses: [
+      {
+        200: {
+          description: "",
+          responseType: "object",
+          schema: {
+            properties: {
+              message: {
+                type: "string"
+              }
+            }
+          }
+        }
+      }
+    ],
+    parameters: [
+      {
+        name: "id",
+        in: "path",
+        required: true,
+        schema: {
+          type: "number"
+        }
+      }
+    ]
+  })
+  public async remove(req: _Request, res: Response): Promise<unknown> {
+    const {
+      id
+    } = req.params;
+
+    const remove = await Example.destroy({
+      where: {
+        id
+      }
+    });
+
+    return {
+      message: "Deleted successfully."
+    };
   }
 }
